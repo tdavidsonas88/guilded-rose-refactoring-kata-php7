0862bc191d2808fe6035e0de559d21119ac83569
tadas <tdavidsonas88@gmail.com>
2019-07-05 22:51:43 +0300

    removed always true or always false if statements

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -33,16 +33,14 @@ final class GildedRose {
             } else {
                 if ($item->quality < 50) {
                     $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                    if ($item->sell_in < 11) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
                         }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                    }
+                    if ($item->sell_in < 6) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
                         }
                     }
                 }
@@ -53,7 +51,7 @@ final class GildedRose {
             }
 
             if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
+                if (true) {
                     if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->quality > 0) {
                             if ($item->name != 'Sulfuras, Hand of Ragnaros') {
@@ -62,10 +60,6 @@ final class GildedRose {
                         }
                     } else {
                         $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
                     }
                 }
             }
@@ -77,21 +71,10 @@ final class GildedRose {
             $item->sell_in = $item->sell_in - 1;
 
             if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
+
                     if ($item->quality < 50) {
                         $item->quality = $item->quality + 1;
                     }
-                }
             }
         }
 
