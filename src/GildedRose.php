8e1cb298daa5b167e0e99b961a90511c3122f9d1
tadas <tdavidsonas88@gmail.com>
2019-07-06 12:01:16 +0300

    writing elseif where possible

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -39,9 +39,7 @@ final class GildedRose {
                     $item->quality = $item->quality + 1;
                 }
             }
-        } else {
-
-            if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+        } else if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality < 50) {
                     $item->quality = $item->quality + 1;
                     if ($item->sell_in < 11) {
@@ -61,40 +59,32 @@ final class GildedRose {
                 if ($item->sell_in < 0) {
                     $item->quality = $item->quality - $item->quality;
                 }
-            } else {
-                if ($item->name == 'Sulfuras, Hand of Ragnaros') {
+        } else if ($item->name == 'Sulfuras, Hand of Ragnaros') {
+                if ($item->quality > 0) {
+                    $item->quality = 80;
+                }
+                if ($item->sell_in < 0) {
                     if ($item->quality > 0) {
-                        $item->quality = 80;
-                    }
-                    if ($item->sell_in < 0) {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    }
-                } else {
-                    if ($item->quality > 0) {
-                        $item->quality = $item->quality - 1;
-                    }
-
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->sell_in = $item->sell_in - 1;
-                    }
-
-                    if ($item->sell_in < 0) {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
+                        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                            $item->quality = $item->quality - 1;
                         }
                     }
                 }
+        } else {
+            if ($item->quality > 0) {
+                $item->quality = $item->quality - 1;
             }
 
+            $item->sell_in = $item->sell_in - 1;
 
+            if ($item->sell_in < 0) {
+                if ($item->quality > 0) {
+                    if (true) {
+                        $item->quality = $item->quality - 1;
+                    }
+                }
+            }
         }
-
 
     }
 }
